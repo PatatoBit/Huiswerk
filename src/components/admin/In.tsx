94d0ea7833d368300dbb18baabc75a2674c1ba9b
Patato <pathonbusiness@gmail.com>
2022-12-03 17:12:41 +0700

    logout

--- src/components/admin/In.tsx
+++ src/components/admin/In.tsx
@@ -1,7 +1,20 @@
 import React from "react";
+import { SignOut } from "../../lib/auth";
 
 function In() {
-  return <h1>Logged in!</h1>;
+  return (
+    <>
+      <h1>Logged in! </h1>
+      <h1>Show dashboard here</h1>
+
+      <button
+        className="mt-2 rounded-md bg-[#F35B04] px-8 py-3 text-xl font-semibold"
+        onClick={SignOut}
+      >
+        <div className="text-[#ffffff]">Log out</div>
+      </button>
+    </>
+  );
 }
 
 export default In;
--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -24,3 +24,7 @@ export function SignIn(
       console.log(errorCode, errorMessage);
     });
 }
+
+export function SignOut() {
+  auth.signOut();
+}
