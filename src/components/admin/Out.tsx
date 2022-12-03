6b321debd76f058abbbdf56d394386fd8767f3b3
Patato <pathonbusiness@gmail.com>
2022-12-03 18:33:13 +0700

    login form

--- src/components/admin/Out.tsx
+++ src/components/admin/Out.tsx
@@ -8,26 +8,28 @@ function Out() {
 
   return (
     <>
-      <input
-        type="text"
-        className="my-2 mt-4 rounded-md px-6 py-4"
-        placeholder="Email"
-        value={email}
-        onChange={(event) => setEmail(event.target.value)}
-      />
-      <input
-        type="text"
-        className="my-2 rounded-md px-6 py-4"
-        placeholder="Password"
-        onChange={(event) => setPassword(event.target.value)}
-      />
+      <form className="flex flex-col">
+        <input
+          type="text"
+          className="my-2 mt-4 rounded-md px-6 py-4"
+          placeholder="Email"
+          value={email}
+          onChange={(event) => setEmail(event.target.value)}
+        />
+        <input
+          type="text"
+          className="my-2 rounded-md px-6 py-4"
+          placeholder="Password"
+          onChange={(event) => setPassword(event.target.value)}
+        />
 
-      <button
-        className="mt-2 rounded-md bg-[#F35B04] px-8 py-3 text-xl font-semibold"
-        onClick={(e) => SignIn(e, email, password)}
-      >
-        <div className="text-[#ffffff]">Login</div>
-      </button>
+        <button
+          className="mt-2 rounded-md bg-[#F35B04] px-8 py-3 text-xl font-semibold"
+          onClick={(e) => SignIn(e, email, password)}
+        >
+          <div className="text-[#ffffff]">Login</div>
+        </button>
+      </form>
     </>
   );
 }
